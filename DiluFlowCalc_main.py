510f393baba5d3cd9e72f5f9ec9e508bfd0f8839
NATRIST-0 <tristgayrard@gmail.com>
2025-07-07 10:16:06 +0200

    minor changes
    remade the background, and created a better looking icon.

--- DiluFlowCalc_main.py
+++ DiluFlowCalc_main.py
@@ -2,8 +2,18 @@
 # author: Tristan Gayrard
 
 """
-Volumetric Flows App
+DiluFlowCalc App
+
+--- A GUI tool for calculating volumetric gas dilution flows ---
 """
+
+import sys
+import os
+ 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
 import numpy as np
 import tkinter as tk
@@ -41,7 +51,6 @@ def calculate():
     Vdil = Vdil * 1000  # From SLPM to SCCM
 
     percent_val = percent_val / 100
-    Vgas_max = (Vcalmax_val - Vcalmax_val * percent_val) * 1000  # From SLPM to SCCM
     Vgas_min = Vcalmax_val * percent_val
 
     Uflow = abs(1 - slope_value)
@@ -49,16 +58,17 @@ def calculate():
     U = Uflow + Utolerence
 
     for w, V in zip(wdil_array, Vdil):
-        if V > 0:  # Only proceed if V is non-negative
-            if V <= Vgas_min or V >= Vgas_max:
-                result.insert(tk.END, f"\nFor Cdil = {w} ± {w*U:.0f} ppb, Vcal = {V:.3f} SCCM, outside the\ninstrument's confidence zone")
+        if V > 0:  # Only proceed if V is positive
+            if V <= Vgas_min:
+                result.insert(tk.END, f"\nFor Cdil = {w} ± {w*U:.0f} ppb, Vcal = {V:.3f} SCCM, check the\ninstrument's confidence zone")
             else:
                 result.insert(tk.END, f"\nFor Cdil = {w} ± {w*U:.0f} ppb, Vcal = {V:.3f} SCCM")
             result.insert(tk.END, "\n")
 
 root = tk.Tk()
-root.title("Determination of the Volumetric Flow of the Cylinder of Air V3")
+root.title("DiluFlowCalc")
 root.geometry('1152x648')
+root.iconbitmap(resource_path("DFC_icon.ico"))
 
 def resize_image(event):
     new_width = event.width
@@ -67,39 +77,39 @@ def resize_image(event):
     photo = ImageTk.PhotoImage(image)
     label.config(image = photo)
     label.image = photo
-    
-image = Image.open("App_Dil_Bkr.png")
+
+image = Image.open(resource_path("DFC_bg.png"))
 copy_of_image = image.copy()
 photo = ImageTk.PhotoImage(image)
 label = ttk.Label(root, image = photo)
 
 # Create widgets
 wGAS = Text(root, height=1, width=8, font=('Sans-serif', 18))
-wGAS.place(relx=0.154, rely=0.85, relwidth=0.05, relheight=0.04)
+wGAS.place(relx=0.143, rely=0.85, relwidth=0.07, relheight=0.04)
 
 wdil = Text(root, height=1, width=10, font=('Sans-serif', 18))
-wdil.place(relx=0.49, rely=0.21, relwidth=0.05, relheight=0.05)
+wdil.place(relx=0.49, rely=0.215, relwidth=0.09, relheight=0.045)
 
 Vair = Text(root, height=1, width=7, font=('Sans-serif', 18))
-Vair.place(relx=0.155, rely=0.056, relwidth=0.04, relheight=0.04)
+Vair.place(relx=0.148, rely=0.056, relwidth=0.06, relheight=0.04)
 
 Vcalmax = Text(root, height=1, width=7, font=('Sans-serif', 18))
-Vcalmax.place(relx=0.23, rely=0.38, relwidth=0.04, relheight=0.04)
+Vcalmax.place(relx=0.22, rely=0.38, relwidth=0.06, relheight=0.04)
 
 step = Text(root, height=1, width=6, font=('Sans-serif', 18))
-step.place(relx=0.53, rely=0.765, relwidth=0.04, relheight=0.04)
+step.place(relx=0.52, rely=0.760, relwidth=0.06, relheight=0.04)
 
 Range = Text(root, height=1, width=6, font=('Sans-serif', 18))
-Range.place(relx=0.65, rely=0.765, relwidth=0.04, relheight=0.04)
+Range.place(relx=0.648, rely=0.760, relwidth=0.05, relheight=0.04)
 
 percent = Text(root, height=1, width=8, font=('Sans-serif', 18))
-percent.place(relx=0.735, rely=0.765, relwidth=0.05, relheight=0.04)
+percent.place(relx=0.735, rely=0.760, relwidth=0.07, relheight=0.04)
 
 slope = Text(root, height=1, width=7, font=('Sans-serif', 18))
-slope.place(relx=0.87, rely=0.765, relwidth=0.05, relheight=0.04)
+slope.place(relx=0.87, rely=0.760, relwidth=0.06, relheight=0.04)
 
 result = Text(root, height=12, width=55, font=('Sans-serif', 18))
-result.place(relx=0.466, rely=0.375, relwidth=0.47, relheight=0.38)
+result.place(relx=0.467, rely=0.375, relwidth=0.45, relheight=0.38)
 
 button = Button(root, text='SEND', relief=tk.RAISED, font=('Sans-serif', 18), command=calculate)
 button.place(relx=0.515, rely=0.9)
